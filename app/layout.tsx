f74f82b3822f7657afd1fc2b6fec5e888c78ca31
韩东遥 <handongyao@orca-tech.com.cn>
2025-03-07 11:11:59 +0800

    feat:添加logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,16 @@ const LocaleLayout = ({
   return (
     <html lang={locale ?? 'en'} className="h-full">
       <body className="h-full">
+        <div className="fixed bottom-4 right-8 z-50">
+          <div className="flex items-center gap-2">
+            <Image
+              src={Logo}
+              alt="Logo"
+              width={200}  // 根据需要调整尺寸
+              height={68}
+            />
+          </div>
+        </div>
         <div className="overflow-x-auto">
           <div className="w-screen h-screen min-w-[300px]">
             {children}
